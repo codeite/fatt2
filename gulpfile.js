14c072f0d8e87934b99a415404f4fdaa4face204
Sam Plews <the@codeite.com>
2014-11-04 13:37:57 +0000

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp')
+var nodemon = require('gulp-nodemon')
+
+gulp.task('startServer', function() {
+  nodemon({
+    script: 'app.js',
+    ext: 'html js',
+    env: {
+      'NODE_ENV': 'development'
+    }
+  }).on('restart', function() {
+    return console.log('restarted!');
+  });
+
+});
+
+gulp.task('default', ['startServer'], function(){
+  gulp.watch ['**/*.js']
+});
