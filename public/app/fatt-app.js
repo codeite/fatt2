1092ee2d48c8b0487558c2695463241c6d031999
Sam Plews <the@codeite.com>
2014-11-15 01:28:38 +0000

    Moved most api calls into service

--- public/app/fatt-app.js
+++ public/app/fatt-app.js
@@ -1,315 +1,212 @@
-var app = angular.module('fatt',['ui.bootstrap'])
+angular.module('fatt')
+  .controller('MainCtrl', ['$scope', '$http', 'faApi', function($scope, $http, faApi) {
+    var commonRecords = [];
 
-app.controller('MainCtrl', function($scope, $http) {
-  var projects = {};
-  var tasks = {};
-  var contacts = {};
-  var activeProjects = undefined;
-  var commonRecords = [];
-  var me = undefined;
+    faApi.getMe(function(foundMe) {$scope.me = foundMe} );
 
-  getMe(function(foundMe){$scope.me = foundMe});
+    $scope.modalShown = false;
+    $scope.commonRecords = commonRecords;
 
-  $scope.modalShown = false;
-  $scope.commonRecords = commonRecords;
+    var today = moment().utc().startOf('day');
+    var faDateFormat = "YYYY-MM-DD";
+    $scope.today = today.day();
 
-  var today = moment().utc().startOf('day');
-  var faDateFormat = "YYYY-MM-DD";
-  $scope.today = today.day();
+    var month = today.clone().startOf('month')
 
-  var month = today.clone().startOf('month')
+    $scope.month = month
+    $scope.monthName = month.format("MMMM YYYY")
 
-  $scope.month = month
-  $scope.monthName = month.format("MMMM YYYY")
+    var offset = month.isoWeekday() - 1
+    var firstDay = month.clone().subtract(offset, 'days')
+    var currentDate = firstDay.clone();
+    var lastDay = firstDay.clone().add(6, 'weeks')
 
-  var offset = month.isoWeekday() - 1
-  var firstDay = month.clone().subtract(offset, 'days')
-  var currentDate = firstDay.clone();
-  var lastDay = firstDay.clone().add(6, 'weeks')
+    var weeks = []
+    var database = {}
 
-  var weeks = []
-  var database = {}
+    for(var weekIndex=0; weekIndex<6; weekIndex++) {
+      var week = []
+      for (var i=0; i<7; i++) {
+        var name = currentDate.format(faDateFormat);
 
-  for(var weekIndex=0; weekIndex<6; weekIndex++) {
-    var week = []
-    for (var i=0; i<7; i++) {
-      var name = currentDate.format(faDateFormat);
+        var thisMonth = currentDate.month() == month.month();
 
-      var thisMonth = currentDate.month() == month.month();
+        var status = '';
 
-      var status = '';
-
-      if(currentDate.isoWeekday() >= 6) {
-        status = 'warning'
-      }
-
-      if(currentDate.format(faDateFormat) == today.format(faDateFormat)) {
-        status = 'active text-primary'
-      }
-
-      var dayObject = {
-        date: currentDate.clone(),
-        day: currentDate.format("Do"),
-        total: 0,
-        records: [],
-        name: name,
-        status: status,
-        thisMonth: thisMonth?'':'text-muted'
-      };
-      week.push(dayObject)
-      database[name] = dayObject
-      dayObject.add = function() {
-        var currentDayObject = dayObject;
-        return function(record) {
-
-          addRecordLike(currentDayObject, record);
-
-
+        if(currentDate.isoWeekday() >= 6) {
+          status = 'warning'
         }
-      }();
 
-      currentDate.add(1, 'days')
+        if(currentDate.format(faDateFormat) == today.format(faDateFormat)) {
+          status = 'active text-primary'
+        }
+
+        var dayObject = {
+          date: currentDate.clone(),
+          day: currentDate.format("Do"),
+          total: 0,
+          records: [],
+          name: name,
+          status: status,
+          thisMonth: thisMonth?'':'text-muted'
+        };
+        week.push(dayObject)
+        database[name] = dayObject
+        dayObject.add = function() {
+          var currentDayObject = dayObject;
+          return function(record) {
+
+            addRecordLike(currentDayObject, record);
+
+
+          }
+        }();
+
+        currentDate.add(1, 'days')
+      }
+      weeks.push(week)
     }
-    weeks.push(week)
-  }
 
-  function deleteTimeslip(timeslipUrl) {
-    $http.delete(timeslipUrl).success( function(data) {
-      readTimeslips();
-    });
-  }
-
-  function addRecordLike(day, record) {
-    getMe(function(me){
-
-      console.log(me);
-      console.log(day);
-      console.log(record);
-
-      var timeslip = {
-        timeslip: {
-          user: me.url,
-          project: record.projectUrl,
-          task: record.taskUrl,
-          dated_on: day.name,
-          hours: record.hours,
-          comment: 'Created using fatt'
-        }
-      };
-
-      console.log(timeslip);
-      $http.post('/freeagent/timeslips', timeslip).success( function(data) {
+    function deleteTimeslip(timeslipUrl) {
+      $http.delete(timeslipUrl).success( function(data) {
         readTimeslips();
       });
-    });
-  }
+    }
 
-  // '/freeagent/users/me'
-  function getMe(callback){
+    function addRecordLike(day, record) {
+      faApi.getMe(function(me){
 
-    if(Array.isArray(me)) {
-      me.push(callback)
-    } else if(typeof(me) == 'object') {
-      if(typeof(callback) == 'function') {
-        callback(me);
+        console.log(me);
+        console.log(day);
+        console.log(record);
+
+        var timeslip = {
+          timeslip: {
+            user: me.url,
+            project: record.projectUrl,
+            task: record.taskUrl,
+            dated_on: day.name,
+            hours: record.hours
+          }
+        };
+
+        console.log(timeslip);
+        $http.post('/freeagent/timeslips', timeslip).success( function(data) {
+          readTimeslips();
+        });
+      });
+    }
+
+    $scope.weeks = weeks
+    readTimeslips();
+
+    function resolveProjectName(record) {
+      faApi.resolveProject(record.projectUrl, function(project) {
+        record.projectName = project.name;
+        record.contactUrl = project.contact;
+        resolveContactName(record)
+      });
+    }
+
+    function resolveTaskName(record) {
+      faApi.resolveTask(record.taskUrl, function(task) {
+        record.taskName = task.name;
+      });
+    }
+
+    function resolveContactName(record) {
+      faApi.resolveContact(record.contactUrl, function(contact) {
+        record.contactName = contact.name;
+      });
+    }
+
+    function addRecordToCommon(record) {
+      var match = commonRecords.reduce(function(found, x) {
+        if(found === null && x.hours == record.hours && x.taskUrl == record.taskUrl) {
+          return x
+        }
+        return found;
+      }, null);
+
+      if(match === null) {
+        record.useCount++;
+        commonRecords.push(record)
+      } else {
+        match.useCount++;
+
       }
-    } else {
-      me = [];
-      me.push(callback)
-
-      $http.get('/freeagent/users/me').success(function(data) {
-        var callbacks = me;
-        me = data.user
-
-        for(var idx=0; idx<callbacks.length; idx++) {
-          getMe(callbacks[idx]);
-        }
-      });
     }
-  }
 
+    function readTimeslips() {
+      $http.get('/freeagent/timeslips?from_date='+firstDay.format(faDateFormat)+'&to_date='+lastDay.format(faDateFormat)).success(function(data){
+        console.log("Done reading timeslips");
 
-  $scope.weeks = weeks
-  readTimeslips();
-
-  function resolveProjectName(record) {
-    var project = projects[record.projectUrl];
-
-    if(Array.isArray(project)) {
-      project.push(record)
-    } else if(typeof(project) == 'object') {
-      record.projectName = project.name;
-      record.contactUrl = project.contact;
-      resolveContactName(record)
-    } else {
-      projects[record.projectUrl] = [];
-      projects[record.projectUrl].push(record)
-
-      $http.get(record.projectUrl).success(function(data) {
-        var records = projects[record.projectUrl];
-        projects[record.projectUrl] = data.project
-
-        for(var idx=0; idx<records.length; idx++){
-          resolveProjectName(records[idx]);
+        for(var idx in database) {
+          var day = database[idx];
+          day.records = [];
+          day.total = 0;
         }
-      });
 
-    }
-  }
+        var timeSlips = data;
+        var index;
 
-  function resolveTaskName(record) {
-    var task = tasks[record.taskUrl];
+        for(var index in data.timeslips) {
+          var timeslip = data.timeslips[index]
 
-    if(Array.isArray(task)) {
-      task.push(record)
-    } else if(typeof(task) == 'object') {
-      record.taskName = task.name
-    } else {
-      tasks[record.taskUrl] = [];
-      tasks[record.taskUrl].push(record)
+          var obj = database[timeslip.dated_on];
 
+          if(obj) {
 
-      $http.get(record.taskUrl).success(function(data) {
-        var records = tasks[record.taskUrl];
-        tasks[record.taskUrl] = data.task
+            obj.total += +timeslip.hours
 
-        for(var idx=0; idx<records.length; idx++){
-         resolveTaskName(records[idx]);
+            var record = {
+              timeslipUrl: timeslip.url,
+              projectUrl: timeslip.project,
+              taskUrl: timeslip.task,
+              projectName: "_",
+              taskName: "_",
+              contactName: "_",
+              hours: +timeslip.hours,
+              useCount: 0,
+              delete: function(){
+                if(confirm("Delete this?"))
+                  deleteTimeslip(this.timeslipUrl);
+              }
+            }
+
+            addRecordToCommon(record);
+
+            obj.records.push(record)
+
+            resolveProjectName(record)
+            resolveTaskName(record)
+          }
+        }
+
+        for(var idx in database) {
+          var day = database[idx];
+          calcStatus(day)
         }
       });
 
-    }
-  }
+      function calcStatus(day) {
+          faApi.getActiveProjects(function(activeProjects) {
+            var requiredHours = 0;
+            for(var idx = 0; idx < activeProjects.projects.length; idx++) {
+              var x = +(activeProjects.projects[idx].hours_per_day);
+              if(x > requiredHours)
+                requiredHours = x;
+            }
 
-  function resolveContactName(record) {
-    var contact = contacts[record.contactUrl];
+            if(day.date < today && day.date.isoWeekday() < 6){
 
-    if(Array.isArray(contact)) {
-      contact.push(record)
-    } else if(typeof(contact) == 'object') {
-      record.contactName = contact.organisation_name
-    } else {
-      contacts[record.contactUrl] = [];
-      contacts[record.contactUrl].push(record)
-
-      $http.get(record.contactUrl).success(function(data) {
-        var records = contacts[record.contactUrl];
-        contacts[record.contactUrl] = data.contact
-
-        for(var idx=0; idx<records.length; idx++){
-         resolveContactName(records[idx]);
-        }
-      });
-
-    }
-  }
-
-  function addRecordToCommon(record) {
-    var match = commonRecords.reduce(function(found, x) {
-      if(found === null && x.hours == record.hours && x.taskUrl == record.taskUrl) {
-        return x
-      }
-      return found;
-    }, null);
-
-    if(match === null) {
-      record.useCount++;
-      commonRecords.push(record)
-    } else {
-      match.useCount++;
-
-    }
-  }
-
-  function readTimeslips() {
-    $http.get('/freeagent/timeslips?from_date='+firstDay.format(faDateFormat)+'&to_date='+lastDay.format(faDateFormat)).success(function(data){
-      console.log("Done reading timeslips");
-
-      for(var idx in database) {
-        var day = database[idx];
-        day.records = [];
-        day.total = 0;
-      }
-
-      var timeSlips = data;
-      var index;
-
-      for(var index in data.timeslips) {
-        var timeslip = data.timeslips[index]
-
-        var obj = database[timeslip.dated_on];
-
-        if(obj) {
-
-          obj.total += +timeslip.hours
-
-          var record = {
-            timeslipUrl: timeslip.url,
-            projectUrl: timeslip.project,
-            taskUrl: timeslip.task,
-            projectName: "_",
-            taskName: "_",
-            contactName: "_",
-            hours: +timeslip.hours,
-            useCount: 0,
-            delete: function(){
-              if(confirm("Delete this?"))
-                deleteTimeslip(this.timeslipUrl);
+            if(day.total < requiredHours) {
+              day.status = 'danger'
+            } else {
+              day.status = 'success'
             }
           }
-
-          addRecordToCommon(record);
-
-          obj.records.push(record)
-
-          resolveProjectName(record)
-          resolveTaskName(record)
-        }
-      }
-
-      for(var idx in database) {
-        var day = database[idx];
-        calcStatus(day)
-      }
-    });
-
-    function calcStatus(day) {
-
-      if(Array.isArray(activeProjects)) {
-        activeProjects.push(day)
-      } else if(typeof(activeProjects) == 'object') {
-
-        var requiredHours = 0;
-        for(var idx = 0; idx < activeProjects.projects.length; idx++) {
-          var x = +(activeProjects.projects[idx].hours_per_day);
-          if(x > requiredHours)
-            requiredHours = x;
-        }
-
-        if(day.date < today && day.date.isoWeekday() < 6){
-
-          if(day.total < requiredHours) {
-            day.status = 'danger'
-          } else {
-            day.status = 'success'
-          }
-
-        }
-      } else {
-        activeProjects = [];
-        activeProjects.push(day)
-
-        $http.get('/freeagent/projects?view=active').success(function(data) {
-          var days = activeProjects;
-          activeProjects = data
-
-          for(var idx=0; idx<days.length; idx++) {
-            calcStatus(days[idx]);
-          }
-        });
-
+        });      
       }
     }
-
-  }
-});
+  }]);
---
+++ public/services/fa-api.js
@@ -0,0 +1,70 @@
+angular.module("fatt", ['ui.bootstrap'])
+  .factory("faApi", ["$http", function($http) {
+    var database = {};
+
+    var getMe = function(callback) {
+      getAndCache('/freeagent/users/me', "user", callback);
+    };
+
+    var getActiveProjects = function(callback) {
+      getAndCache('/freeagent/projects?view=active', null, callback);
+    };
+
+    function resolveProject(projectUrl, callback) {
+      getAndCache(projectUrl, "project", callback);
+    };
+
+    function resolveTask(taskUrl, callback) {
+      getAndCache(taskUrl, "task", callback);
+    }
+
+    function resolveContact(contactUrl, callback) {
+      getAndCache(contactUrl, "contact", callback);
+    }
+
+    var getAndCache = function(url, transform, callback) {
+      if(typeof(callback) != "function") {
+        callback = function(){};
+      }
+
+      var value = database[url];
+
+      if(Array.isArray(value)) {
+        value.push(callback);
+      } else if (typeof(value) == 'object') {
+        callback(value);
+      } else {
+        database[url] = []
+        database[url].push(callback);
+
+        $http.get(url).success(function(data){
+          var callbacks = database[url];
+
+          var result;
+          if(transform == null) {
+            result = data;
+          } else  if(typeof(transform) == 'function'){
+            result = transform(data);
+          } else {
+            result = data[transform]
+          }
+
+          database[url] = result;
+
+          for(var idx=0; idx<callbacks.length; idx++){
+            var callback = callbacks[idx];
+            callback(result);
+          }
+        });
+      }
+    };
+
+    return {
+      getMe : getMe,
+      getActiveProjects : getActiveProjects,
+      resolveProject: resolveProject,
+      resolveTask: resolveTask,
+      resolveContact: resolveContact
+
+    };
+  }])
