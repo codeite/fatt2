e7096cefdaad5793cddf3ee07ae30ecc65d7bad3
Sam Plews <the@codeite.com>
2015-04-28 19:33:33 +0100

    Added auth via query string

--- server/middleware/readUser.js
+++ server/middleware/readUser.js
@@ -4,7 +4,7 @@ module.exports = function (context) {
   var authauth = require('../services/authauth')(context);
 
   return function(req, res, next) {
-    var token = req.cookies.authauth;
+    var token = req.cookies.authauth || req.query.authauth;
 
     if(typeof(token) === 'string' && token !== '') {
       req.user = authauth.verifyToken(token);
