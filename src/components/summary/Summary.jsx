11df67bf170f0730f9782e8086525abd303b3eee
Sam Plews <the@codeite.com>
2017-11-11 01:43:32 +0000

    Add year and month summary

--- src/components/summary/Summary.jsx
+++ src/components/summary/Summary.jsx
@@ -5,10 +5,22 @@ import faApi from '../../services/fa-api'
 
 export const Summary = React.createClass({
   getInitialState () {
-    return {timeslipsByDate: {}}
+    return {
+      timeslipsByDate: {},
+      monthStats: {},
+      yearStats: {}
+    }
   },
   componentWillMount () {
-    faApi.readTimeslips(this.props.year + '-01-01', this.props.year + '-12-31')
+    this.readAndCalc(this.props.year)
+  },
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.year !== this.props.year) {
+      this.readAndCalc(nextProps.year)
+    }
+  },
+  readAndCalc (year) {
+    faApi.readTimeslips(year + '-01-01', year + '-12-31')
       .then(data => {
         console.log('data:', data)
         const projectsAndTasks = new Set()
@@ -32,7 +44,6 @@ export const Summary = React.createClass({
           p[c.url] = c
           return p
         }, {})
-        console.log('rMap:', rMap)
 
         const timeslipsByDate = timeslips.reduce((p, c) => {
           if (!p[c.dated_on]) p[c.dated_on] = []
@@ -42,7 +53,58 @@ export const Summary = React.createClass({
           return p
         }, {})
 
-        this.setState({timeslipsByDate})
+        const monthStats = timeslips.reduce((p, c) => {
+          const date = moment(c.dated_on)
+          if (!p[date.month()]) p[date.month()] = {worked: 0, unworked: 0}
+          const hours = ~~c.hours
+
+          if (c.task.is_billable) {
+            p[date.month()].worked += hours
+          } else {
+            p[date.month()].unworked += hours
+          }
+          return p
+        }, {})
+
+        const yearStats = {
+          workedDays: 0,
+          holidayDays: 0,
+          untrackedDays: 0
+        }
+        const date = moment(year + '-01-01')
+        console.log('monthStats', monthStats)
+
+        while ('' + date.year() === '' + this.props.year) {
+          const day = timeslipsByDate[date.format('YYYY-MM-DD')]
+          console.log('day:', day)
+          if (day) {
+            const dayData = day.reduce((p, c) => {
+              if (c.task.is_billable) {
+                p.w += ~~c.hours
+              } else {
+                p.u += ~~c.hours
+              }
+              return p
+            }, {w: 0, u: 0})
+            console.log('dayData:', dayData)
+            if (dayData.w >= 8) {
+              yearStats.workedDays++
+            } else if (dayData.w >= 4 && dayData.u >= 4) {
+              yearStats.workedDays += 0.5
+              yearStats.holidayDays += 0.5
+            } else if (dayData.u >= 8) {
+              yearStats.holidayDays++
+            } else {
+              yearStats.untrackedDays++
+            }
+          } else {
+            yearStats.untrackedDays++
+          }
+
+          date.add(1, 'days')
+        }
+
+        this.setState({timeslipsByDate, monthStats, yearStats})
       })
   },
 
@@ -117,29 +179,74 @@ export const Summary = React.createClass({
 
     function getStyle () {
       return {
-        textAlign: 'center'
+        textAlign: 'center',
+        width: 'initial'
       }
     }
 
-    return <table>
-      <thead>
-        <tr>
-          <th />
-          {(dow().map((x, i) => {
-            return <th key={i} style={getStyle()}>{x}</th>
-          }))}
-        </tr>
-      </thead>
-      <tbody>
-        {months.map(mIndex => {
-          return <tr key={mIndex}>
-            <th>{day.format('MMM')}</th>
-            {days(mIndex).map(dom =>
-              <td key={dom.id} style={getStyle()} className={calClassName(dom)}>{dom.dayOfMonth}</td>
-            )}
+    const month = mIndex => {
+      return moment(this.props.year + '-' + (mIndex + 1) + '-01')
+    }
+
+    return <div>
+      {this.state.yearStats && <table>
+        <tbody>
+          <tr>
+            <td>Days works:</td>
+            <td>{this.state.yearStats.workedDays}</td>
           </tr>
-        })}
-      </tbody>
-    </table>
+          <tr>
+            <td>Days off:</td>
+            <td>{this.state.yearStats.holidayDays}</td>
+          </tr>
+          <tr>
+            <td>Days untracked:</td>
+            <td>{this.state.yearStats.untrackedDays}</td>
+          </tr>
+        </tbody>
+      </table>}
+
+      <table>
+        <thead>
+          <tr>
+            <th />
+            {(dow().map((x, i) => {
+              return <th key={i} style={getStyle()}>{x}</th>
+            }))}
+          </tr>
+        </thead>
+        <tbody>
+          {months.map(mIndex => {
+            return <tr key={mIndex}>
+              <th>{day.format('MMM')}</th>
+              {days(mIndex).map(dom =>
+                <td key={dom.id} style={getStyle()} className={calClassName(dom)}>{dom.dayOfMonth}</td>
+              )}
+            </tr>
+          })}
+        </tbody>
+      </table>
+      <hr />
+      <table>
+        <thead>
+          <tr>
+            <th>Month</th>
+          </tr>
+        </thead>
+        <tbody>
+          {months.map(mIndex => {
+            return <tr key={mIndex} >
+              <th>{month(mIndex).format('MMM')}</th>
+              {this.state.monthStats[mIndex] && <td>
+                worked: {this.state.monthStats[mIndex].worked / 8}
+                {' '}
+                unworked: {this.state.monthStats[mIndex].unworked / 8}
+              </td>
+              }
+            </tr>
+          })}
+        </tbody>
+      </table>
+    </div>
   }
 })
